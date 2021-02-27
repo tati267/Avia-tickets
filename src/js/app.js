4705deec09c530860946e1d137c5b35a6b7a19e7
Tatiana Panteleeva <tatiana.panteleeva2018@gmail.com>
2021-02-27 13:23:11 +1100

    second stage

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,8 @@
+/*import api from './services/apiService';
+
+api.countries().then(res=> console.log(res));
+api.cities().then(res=> console.log(res));
+*/
 import locations from './store/locations';
 
 locations.init().then(res => {
@@ -5,3 +10,4 @@ locations.init().then(res => {
   console.log(locations);
   console.log(locations.getCitiesByCountryCode('PE'));
 });
+
