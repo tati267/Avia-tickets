ce9016a592a3aec0b4c6b977641b7bb96cb83445
Tatiana Panteleeva <tatiana.panteleeva2018@gmail.com>
2021-03-02 14:10:07 +1100

    app build

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,41 @@
-/*import api from './services/apiService';
-
-api.countries().then(res=> console.log(res));
-api.cities().then(res=> console.log(res));
-*/
 import '../css/style.css';
 import './plugins';
 import locations from './store/locations';
+import formUI from './views/form';
+import ticketsUI from './views/tickets';
+import currencyUI from './views/currency';
 
-locations.init().then(res => {
-  console.log(res);
-  console.log(locations);
-  console.log(locations.getCitiesByCountryCode('PE'));
+document.addEventListener('DOMContentLoaded', e => {
+  const form = formUI.form;
+
+  // Events
+  initApp();
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    onFormSubmit();
+  });
+
+  // handlers
+  async function initApp() {
+    await locations.init();
+    formUI.setAutocompleteData(locations.shortCities);
+  }
+
+  async function onFormSubmit() {
+    const origin = locations.getCityCodeByKey(formUI.originValue);
+    const destination = locations.getCityCodeByKey(formUI.destinationValue);
+    const depart_date = formUI.departDateValue;
+    const return_date = formUI.returnDateValue;
+    const currency = currencyUI.currecyValue;
+
+    await locations.fetchTickets({
+      origin,
+      destination,
+      depart_date,
+      return_date,
+      currency,
+    });
+
+    ticketsUI.renderTickets(locations.lastSearch);
+  }
 });
-
--- src/js/plugins/materialize.js
+++ src/js/plugins/materialize.js
@@ -1,34 +1,35 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
 
-//init select
+// Init select
 const select = document.querySelectorAll('select');
 M.FormSelect.init(select);
 
-export function getSelectInstance(elem){
-    return M.FormSelect.getInstance(elem)
-};
+export function getSelectInstance(elem) {
+  return M.FormSelect.getInstance(elem);
+}
 
-//init autocomplete
+// Init Autocomplete
 const autocomplete = document.querySelectorAll('.autocomplete');
-M.Autocomplete.init(autocomplete,{ 
-    data: {
-          "Apple": null,
-          "Microsoft": null,
-          "Google": 'https://placehold.it/250x250'
-        },
-      });
-
-export function getAutocompleteInstance(elem){
-    return M.Autocomplete.getInstance(elem)
-};
-
-//init date pickers
-const datepickers = document.querySelectorAll('.datepicker');
-M.Datepicker.init(datepickers,{
-    showClearBtn: true
+M.Autocomplete.init(autocomplete, {
+  data: {
+    Apple: null,
+    Microsoft: null,
+    Google: 'https://placehold.it/250x250',
+  },
 });
 
-export function getDatePickersInstance(elem){
-    return M.Datepickers.getInstance(elem)
-};
+export function getAutocompleteInstance(elem) {
+  return M.Autocomplete.getInstance(elem);
+}
+
+// Init datepickers
+const datepickers = document.querySelectorAll('.datepicker');
+M.Datepicker.init(datepickers, {
+  showClearBtn: true,
+  format: 'yyyy-mm',
+});
+
+export function getDatePickerInstance(elem) {
+  return M.Datepicker.getInstance(elem);
+}
